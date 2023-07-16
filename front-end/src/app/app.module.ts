95f6461c4b9fefe5b4793f149b5173ac4ef84f77
GeorgeKonrad29 <jorge.lopez2@utp.edu.co>
2023-07-16 14:21:57 -0500

    stable version updated

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { AdminComponent } from './components/admin/admin.component';
 import { PaymentComponent } from './components/payment/payment.component';
 import { ProductsComponent } from './components/cart/products/products.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,11 +26,12 @@ import { ProductsComponent } from './components/cart/products/products.component
     ProductComponent,
     AdminComponent,
     PaymentComponent,
-    ProductsComponent
+    ProductsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
