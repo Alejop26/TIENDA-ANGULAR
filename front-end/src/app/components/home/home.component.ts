250301591b3fc1014746d924c0bb54271f6a424b
Arnovii <arnovi.jimenez@utp.edu.co>
2023-07-16 18:11:11 -0500

    Funcionalidad de la API RickAndMorty acoplada en home, funcionalidad de las rutas integrada en app.module

--- front-end/src/app/components/home/home.component.ts
+++ front-end/src/app/components/home/home.component.ts
@@ -1,33 +1,35 @@
-import { Component} from '@angular/core';
-
-
+import { Component } from '@angular/core';
+import { ApiService } from './service/api.service';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 
-
-
 export class HomeComponent {
 
-  //Inicializamos las variables que puedan utilizarse en el HTML
-  protected idProduct: number;
-  protected nameProduct: string;
-  protected priceProduct: number;
-  protected imageProduct: string;
-  protected descriptionProduct: string;
+  constructor(private apiService: ApiService , private router:Router) {}
 
+  data:any[] = [];
 
-
-  //Le damos valores a las variables
-  constructor(){
-    this.nameProduct="Pablo";
-    this.idProduct = 1 
-    this.priceProduct = 100;
-    this.imageProduct = "../public/pablo.jpg";
-    this.descriptionProduct = "Pablo es un perro muy jugueton y cariñoso, le encanta jugar con la pelota y que le rasquen la panza";
-
+  ngOnInit(): void {
+    this.llenarData();
   }
+
+  llenarData(){
+    this.apiService.getData().subscribe(data => {
+      this.data = data;
+      console.log(this.data);
+    })
+  }
+
+  //Funcion encargada de que al ejecutarse con el evento click, redirija a la pagina de producto.
+  viewProductPage(id: number){
+    this.router.navigate([`/product/${id}`]);
+  }
+
+
+
 
 }
