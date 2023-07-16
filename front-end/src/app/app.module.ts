bd70aa4d35f0229cfbbf13243c1675ef305b9fcf
XDBRAN98 <jhans.velez@utp.edu.co>
2023-07-16 14:45:51 -0500

    pre unión con la rama de login

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -31,6 +32,7 @@ import { PaymentComponent } from './components/payment/payment.component';
     AppRoutingModule,
     MatToolbarModule,
     MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
