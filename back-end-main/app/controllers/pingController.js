134da3d625f5889a187ace44141344234d5cc55e
Emaa2005 <jualparraal21@gmail.com>
2023-07-16 18:12:41 -0500

    Comments

--- back-end-main/app/controllers/pingController.js
+++ back-end-main/app/controllers/pingController.js
@@ -4,10 +4,12 @@ const pingController = async (req, res) => {
     try {
         const results = await User.findAll();
         console.log(results);
-        res.json(results);
+        res.json(results);//se envia la respuesta en formato json 
     } catch (error) {
         console.log(error);
     }
 };
 
 export default pingController;
+
+//este código muestra un controlador para una ruta específica que realiza una consulta a la base de datos para obtener todos los registros de usuarios y los envía como respuesta en formato JSON.
