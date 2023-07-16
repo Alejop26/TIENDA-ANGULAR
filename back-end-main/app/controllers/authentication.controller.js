a3712a7418568e18cb17b85151d27aedbdbbf1c5
andrepalacio <andres.palacio@utp.edu.co>
2023-07-16 12:58:43 -0500

    recovery

--- back-end-main/app/controllers/authentication.controller.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const loginController = async (req, res) => {
-  return res.send(
-    jwt.sign({ test: "payload" }, "secretKey", { expiresIn: "1h" })
-  );
-};
-
-export default loginController;
--- back/app/routes/authentication/authentication.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import loginController from '../../controllers/authentication.controller.js';
-
-const authenticationRouter = express.Router();
-
-// JWT authentication middleware
-authenticationRouter.post('/login', loginController);
-
-export default authenticationRouter;
