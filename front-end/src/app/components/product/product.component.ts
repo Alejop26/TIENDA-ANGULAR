631127fbe261c7853ab7dc805d8e530e3c25c062
andrepalacio <andres.palacio@utp.edu.co>
2023-07-17 02:41:53 -0500

    product view with images and buttons

--- front-end/src/app/components/product/product.component.ts
+++ front-end/src/app/components/product/product.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router'
 import {ProductsService} from '../../services/products.service'
-import { CarouselComponent } from './carousel/carousel.component';
 
 @Component({
   selector: 'app-product',
@@ -10,10 +9,11 @@ import { CarouselComponent } from './carousel/carousel.component';
 })
 
 export class ProductComponent implements OnInit {
-  idProducto = 0;
-  producto: any = {}
+  idProducto: number;
+  producto: any = {};
   images: string[] = [];
   selectedImage: string;
+  orderQuantity: number = 1;
 
   constructor(private route: ActivatedRoute, private productsService: ProductsService ) {}
 
@@ -29,13 +29,47 @@ export class ProductComponent implements OnInit {
     this.productsService.getOne(this.idProducto).subscribe(data => {
       this.producto = data;
       console.log(data)
-      this.images = [this.producto.productImages[0], this.producto.productImages[1], this.producto.productImages[2]];
+      console.log(this.producto.productImages[0].imageURL)
+      this.images = [this.producto.productImages[0].imageURL, this.producto.productImages[1].imageURL, this.producto.productImages[2].imageURL];
       this.selectedImage = this.images[0];
     })
   }
 
   changeImage(imageUrl: string): void {
     this.selectedImage = imageUrl;
+  }
+
+  comparationMaxAmount():boolean {
+    // if (this.orderQuantity >= this.producto.stock)
+    // {
+    //   this.orderQuantity = this.producto.stock;
+    //   return false;
+    // }
+    return false;
+  };
+
+  minAmountHandler():boolean {
+    if(this.orderQuantity <= 1)
+    {
+      this.orderQuantity = 1;
+      return true;
+    }
+    return false;
+  }
+    
+  decreaseClickHandler()   {
+      this.orderQuantity--;
+     
+    }
+  increaseClickHandler()   {
+      this.orderQuantity++;
+  }
+
+  addToCart(): void {
+  //   this.productsService.addToCart(this.idProducto, this.orderQuantity).subscribe(data => {
+  //     console.log(data);
+  //   })
+    console.log("ordering ...", this.orderQuantity)
   }
 }
 
