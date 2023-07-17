e94dd8cda4747ecb7742701fda83c938b71c30ae
andrepalacio <andres.palacio@utp.edu.co>
2023-07-17 16:35:10 -0500

    update and delete

--- front-end/src/app/components/admin/admin.component.ts
+++ front-end/src/app/components/admin/admin.component.ts
@@ -9,6 +9,9 @@ interface Product {
   stock: number;
   stockMin: number;
   stockMax: number;
+  product: { 
+    price: number;
+  };
 }
 
 interface Image {
@@ -23,7 +26,7 @@ interface Image {
 export class AdminComponent implements OnInit {
   products: Product[] = [];
   images: Image[] = [];
-  apiUrl = "http://localhost:8080/api/inventory";
+  apiUrl = "http://localhost:8080/api";
 
   constructor(private http: HttpClient, private inventoryService: InventoryService) {}
 
@@ -39,9 +42,9 @@ export class AdminComponent implements OnInit {
       console.log(this.products)
       for (let index = 0; index < this.products.length; index++) {
         const element = this.products[index];
-        this.http.get<Image>(`http://localhost:8080/api/images/${element.productID}`).subscribe((data: any) => {
+        this.http.get<Image>(`${this.apiUrl}/images/${element.productID}`).subscribe((data: any) => {
           this.images.push(data[0].imageURL);
-          console.log(this.images);
+          // console.log(this.images);
         })
       }
     });
@@ -49,11 +52,21 @@ export class AdminComponent implements OnInit {
 // ------------------------------------------------------------
   saveChanges(product: Product) {
     // Lógica para guardar los cambios del producto
+    this.http.put<Product>(`${this.apiUrl}/inventory/product/${product.productID}`, product).subscribe((data: any) => {
+      console.log(data);
+    })
     console.log('Product Saved:', product);
   }
 
   deleteProduct(product: Product) {
     // Lógica para eliminar el producto
+    console.log(product.productID);
+    this.http.delete<Product>(`${this.apiUrl}/images/${product.productID}`).subscribe((data: any) => {
+      console.log(data);
+    });
+    this.http.delete<Product>(`${this.apiUrl}/products/${product.productID}`).subscribe((data: any) => {
+      console.log(data);
+    });
     console.log('Product Deleted:', product);
   }
 
