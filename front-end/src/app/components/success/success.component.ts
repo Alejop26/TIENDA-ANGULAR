159be302deee0e25fc654b4948045586d369f1bb
SMejia115 <s.mejia6@utp.edu.co>
2023-07-18 02:58:11 -0500

    Any Changes

--- front-end/src/app/components/success/success.component.ts
+++ front-end/src/app/components/success/success.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 
 @Component({
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./success.component.css']
 })
 export class SuccessComponent {
+  constructor( private router:Router){}
 
+  inicio(){
+    this.router.navigate(['/']);
+  }
 }
